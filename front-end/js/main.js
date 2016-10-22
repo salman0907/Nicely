1ebe6f4f650009867daf0b785c3bec0ae2b0d58d
Jay Khatri <khatri.florida@gmail.com>
2016-10-22 17:40:15 -0500

    oauht token needed

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -59,11 +59,11 @@ statusChangeCallback(response);
 
 // Load the SDK asynchronously
 (function(d, s, id) {
-var js, fjs = d.getElementsByTagName(s)[0];
-if (d.getElementById(id)) return;
-js = d.createElement(s); js.id = id;
-js.src = "https://connect.facebook.net/en_US/all.js";
-fjs.parentNode.insertBefore(js, fjs);
+	var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "https://connect.facebook.net/en_US/all.js";
+		fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
 // Here we run a very simple test of the Graph API after login is
